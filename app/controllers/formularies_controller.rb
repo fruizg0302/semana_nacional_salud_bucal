24ef14a836dfbe9177c1f1c388a19345f21f36a3
fruizg0302 <fruizg0302@icloud.com>
2014-11-20 14:59:01 -0600

    Working on state results

--- app/controllers/formularies_controller.rb
+++ app/controllers/formularies_controller.rb
@@ -7,7 +7,7 @@ class FormulariesController < ApplicationController
       render 'jurisdictional_results' # renders app/views/formularies/index/_product.html.erb
     elsif can? :view_state_results, @user
       @formularios = Formulary.resultados_estatales(User.users_from_jurisdiction(current_user.jurisdiction_id))
-      render partial: 'state_results'
+      render 'state_results'
     elsif can? :view_national_results, @user
       #@formularios = Formulary.resultados_nacionales
     else
